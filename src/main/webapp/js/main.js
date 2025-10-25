befc242cedf45b0d018f85d0850b8af3e7d3ae50
Muhammed Emir Cinan <118529161+CinanEmir@users.noreply.github.com>
2025-10-25 20:29:24 +0300

    Update main.js

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,14 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     const welcomeScreen = document.getElementById('welcome-screen');
-    setTimeout(() => {
+    
+    // 1 saniye (1000ms) yerine 2 saniye (2000ms) sonra animasyonu başlat
+    setTimeout(() => { 
         // Hoş Geldiniz ekranını yavaşça soluklaştır
         welcomeScreen.style.opacity = '0';
         welcomeScreen.style.transition = 'opacity 0.5s ease-out';
         
-        // Animasyon bittikten sonra yönlendir
-        setTimeout(() => {
+        // Animasyon (0.5 saniye) bittikten sonra yönlendir
+        setTimeout(() => { 
             window.location.href = 'main-screen.html';
-        }, 200); // 500ms sonra yönlendir
+        }, 500); // Animasyon süresine göre 500ms sonra yönlendir (0.5 saniye)
         
-    }, 1000); // 1 saniye sonra animasyonu başlat
+    }, 2000); // BURASI DEĞİŞTİ: 1000ms'den 2000ms'ye
 });
