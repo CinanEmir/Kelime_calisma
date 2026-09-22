568706eb07ff8f96ac8baac2bbd4887b08fa1553
Muhammed Emir Cinan <22060328stu.omu.edu.tr>
2026-09-22 17:37:03 +0300

    feat: Beyaz kenarliklar kaldirildi, maskable ikon navy gradyan yapildi ve assetlinks.json iki anahtarla guncellendi

--- src/main/webapp/sw.js
+++ src/main/webapp/sw.js
@@ -1,5 +1,5 @@
 // sw.js - KelimePulse Service Worker (Offline Destegi)
-const CACHE_NAME = 'kelimepulse-v6';
+const CACHE_NAME = 'kelimepulse-v7';
 const ASSETS_TO_CACHE = [
   './',
   'index.html',
