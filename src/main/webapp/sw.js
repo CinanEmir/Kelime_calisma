fdcd2897b348d5e922392bfcc6fbe6ac07cdc4c1
Muhammed Emir Cinan <22060328stu.omu.edu.tr>
2026-09-25 02:07:40 +0300

    feat: Splash logo olculeri (%50) dengelendi ve web acilisiyla birebir esitlendi

--- src/main/webapp/sw.js
+++ src/main/webapp/sw.js
@@ -1,5 +1,5 @@
 // sw.js - KelimePulse Service Worker (Offline Destegi)
-const CACHE_NAME = 'kelimepulse-v8';
+const CACHE_NAME = 'kelimepulse-v10';
 const ASSETS_TO_CACHE = [
   './',
   'index.html',
